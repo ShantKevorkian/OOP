51d38e33b0ee34918574977794132bb645fa92c5
Shant Kevorkian <shantkevv@gmail.com>
2021-04-05 17:32:26 +0400

    Modified

--- db.php
+++ db.php
@@ -1,25 +1,19 @@
 <?php
 
-    class Db {
-        
-        private $conn;
+    class Db extends mysqli {
 
         private $where_condition = '';
 
-        function __construct($db_host = "localhost", $db_user = "root", $db_pass = "", $db_name = "internship") {
-            $this->conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-            if ($this->conn->connect_errno) {
-                die("Connection failed: " . "<br>" . $this->conn->connect_error);
+        function __construct() {
+            parent::__construct($db_host = "localhost", $db_user = "root", $db_pass = "", $db_name = "internship");
+            if (mysqli_connect_errno()) {
+                die('Connect Error: ' . mysqli_connect_error());
             }
         }
 
-        function __destruct() {
-		    $this->conn->close();
-	    }
-
         public function select($sql, $all = true){
 
-            $query_select = $this->conn->query($sql);
+            $query_select = parent::query($sql);
 
             if(!$all) {
                 $row = $query_select->fetch_assoc();
@@ -35,19 +29,19 @@
         public function insert($table_name, $data){
             $value_data = '';
             foreach($data as $key => $value) {  
-                $value_data .="'".$this->conn->real_escape_string(htmlspecialchars($value))."', ";  
+                $value_data .="'".parent::real_escape_string(htmlspecialchars($value))."', ";  
             }
             $value_data = substr($value_data, 0, -2);
             $query_insert = "INSERT INTO $table_name (".implode(",", array_keys($data)).") 
                                 VALUES ($value_data)";
-            return $this->conn->query($query_insert);
+            return parent::query($query_insert);
         }
 
         public function update($table_name, $updated_data){
             $data = '';
 
             foreach($updated_data as $key => $value) {  
-                $data .= $key . "='".$this->conn->real_escape_string(htmlspecialchars($value))."', ";  
+                $data .= $key . "='".parent::real_escape_string(htmlspecialchars($value))."', ";  
             }
             $data = substr($data, 0, -2);
             
@@ -55,7 +49,7 @@
             $this->where_condition = '';
 
             $query_update = "UPDATE $table_name SET " . $data . $where;
-            return $this->conn->query($query_update);
+            return parent::query($query_update);
         }
 
         public function delete($table_name){
@@ -63,11 +57,11 @@
             $this->where_condition = '';
 
             $query_delete = "DELETE FROM $table_name" . $where;
-            return $this->conn->query($query_delete);
+            return parent::query($query_delete);
         }
 
         public function where($column, $column_value, $oper = '=') {
-            $column_value = $this->conn->real_escape_string(htmlspecialchars($column_value));
+            $column_value = parent::real_escape_string(htmlspecialchars($column_value));
             if($this->where_condition != '') {
                 $this->where_condition .= " AND $column $oper '$column_value' ";
             }
@@ -78,7 +72,7 @@
         }
 
         public function orWhere($column, $column_value, $oper = '=') {
-            $column_value = $this->conn->real_escape_string(htmlspecialchars($column_value));
+            $column_value = parent::real_escape_string(htmlspecialchars($column_value));
             if($this->where_condition != '') {
                 $this->where_condition .= " OR $column $oper '$column_value' ";
             }
